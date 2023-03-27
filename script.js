580fb64073da6faff9a7322836934b05ba67cd66
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-27 11:31:58 -0300

    aula5

---
+++ script.js
@@ -0,0 +1,49 @@
+// alert simples
+
+//alert ("Olá mundo")
+
+//alert de confirmção 
+//let msg=confirm("Você é um aluno?")
+
+//alert passando valores em campos 
+
+//let msg1=prompt("Digite seu nome")
+//document.write("Meu nome é: ",msg1)
+
+//condição if 
+//if(10>7){
+    //document.write("Dez é maior que sete")
+//}
+//let valor = 20 
+
+//if(valor > 20){
+    //document.write("<br>")
+//document.write ("O " +valor+ " é maior que 10")
+
+//}else { 
+    //document.write("<br>")
+    //document.write("O valor é menor")
+//}
+/*
+let idade = 18
+if (idade <= 12){
+    document.write("É uma criança")
+}else if(idade <= 17){
+    document.write("É um adolescente")
+}else if(idade <= 29){
+    document.write("É um jovem")
+}else{ 
+    document.write("É um adulto")
+}
+*/
+
+let valor=prompt("Digite um valor")
+
+if(valor > 10){
+    document.write("O valor é maior")
+}else if(valor == 10){
+    document.write("O valor é igual")
+}
+else{
+    document.write("O valor é menor")
+}
